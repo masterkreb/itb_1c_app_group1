6b50ef9c53d509f604386c493698c4c3cdca6b6c
Manuel Pritz <manuel.pritz@pritz-it.com>
2025-05-17 14:03:09 +0200

    Refactor FilmPage and add FilmDetailsPage with API integration
    Replaced the static form-based FilmPage with a table view fetching films from the server via a new FilmService API. Added FilmDetailsPage for displaying film details and updated router configuration accordingly.

--- frontend/src/config/routerConfig.tsx
+++ frontend/src/config/routerConfig.tsx
@@ -1,8 +1,9 @@
 import {createBrowserRouter} from "react-router";
 import App from "../App.tsx";
-import FilmPage from "../pages/view/FilmPage.tsx";
-import ActorPage from "../pages/view/ActorPage.tsx";
-import NotFoundPage from "../pages/view/NotFoundPage.tsx";
+import FilmPage from "../pages/FilmPage.tsx";
+import ActorPage from "../pages/ActorPage.tsx";
+import NotFoundPage from "../pages/NotFoundPage.tsx";
+import FilmDetailsPage from "../pages/FilmDetailsPage.tsx";
 
 export const router = createBrowserRouter([
     {
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
             {
                 path: "film",
                 element: <FilmPage />
+            },
+            {
+                path: "film/:id",
+                element: <FilmDetailsPage />
             },
             {
                 path: "actor",
