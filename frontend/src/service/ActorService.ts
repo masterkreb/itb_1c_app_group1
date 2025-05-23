555afd7816f5613e2ba14e09d559e244e95ae321
Rigo Erisk Reyes <eriskbenereyes@gmail.com>
2025-05-23 21:00:38 +0200

    filmService.ts modify ich habe ein sicherheits für '; DROP TABLE category; --). categoryService.ts auch für categoryService.ts

---
+++ frontend/src/service/ActorService.ts
@@ -0,0 +1,34 @@
+const baseUrl = "http://localhost:3000/actor";
+
+export async function getAllActors() {
+    console.log("Start GetActors")
+
+    const response = await fetch(baseUrl, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+    console.log("Got response from server: ", response);
+    if (!response.ok) {
+        console.error("Error while fetching actors: ", response.status);
+        return undefined;
+    }
+
+    const tempActors = await response.json();
+    return tempActors.data;
+}
+
+export async function getActorById(id: number) {
+    console.log("Start GetActorById")
+    const response = await fetch(`${baseUrl}/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+    console.log("Got response from server: ", response);
+    if (!response.ok) {
+        console.error("Error while fetching actor: ", response.status);
+    }
+}
