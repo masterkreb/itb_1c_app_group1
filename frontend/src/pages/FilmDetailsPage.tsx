f755898e23b1d21bac27059d882e12d036906e4b
perdenis <perdomodenis@outlook.com>
2025-05-23 18:06:30 +0200

    getFilmById Funktion integriert

--- frontend/src/pages/FilmDetailsPage.tsx
+++ frontend/src/pages/FilmDetailsPage.tsx
@@ -1,17 +1,38 @@
 // noinspection JSUnusedLocalSymbols
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useParams} from "react-router";
 import JsonView from "@uiw/react-json-view";
+import {Film} from "../types/types.ts"
+import {getFilmById} from "../service/FilmService.ts";
 
 const FilmDetailsPage = () => {
+    const params = useParams<{id: string}>();
+    const [films, setFilms] = React.useState<Film | null>(null);
 
-    const params = useParams();
+    useEffect(() => {
+        if (params.id) {
+            const filmId = Number(params.id);
+            if (isNaN(filmId)) {
+                console.error("Ungültige Film-ID");
+                return;
+            }
+
+            getFilmById(filmId)
+                .then(response => {
+                    setFilms(response);
+                });
+        }
+    }, [params.id]);
 
     return (
         <div>
-            Film Details
-            <JsonView value={params} />
+            <h2>Film Details</h2>
+            {films ? (
+                <JsonView value={films} />
+            ) : (
+                <p>Keine Filmdaten verfügbar</p>
+            )}
         </div>
     );
 };
